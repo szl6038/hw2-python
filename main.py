c1725be51c6fe79def7c0beacd8583d4111589ab
szl6038 <szl6038@psu.edu>
2020-09-09 21:33:19 +0000

    definitions + GP1,2,3

--- main.py
+++ main.py
@@ -30,8 +30,35 @@ def run():
 
   print("Grade point for course 1 is: " +str(GP1))
 
+  Course2grade = input("Enter your course 2 letter grade: ")
 
+  Course2credit = input("Enter your course 2 credit: ")
 
+  GP2 = getGradePoint(Course2grade)
+
+  print("Grade point for course 2 is: " +str(GP2))
+
+  Course3grade = input("Enter your course 3 letter grade: ")
+
+  Course3credit = input("Enter your course 3 credit: ")
+
+  GP3 = getGradePoint(Course3grade)
+
+  print("Grade point for course 3 is: " +str(GP3))
+
+  GPC1 = (GP1*float(Course1credit))
+
+  GPC2 = (GP2*float(Course2credit))
+
+  GPC3 = (GP3*float(Course3credit))
+
+  TGPC = (GPC1+GPC2+GPC3)
+
+  TC = (float(Course1credit)+float(Course2credit)+float(Course3credit))
+
+  GPA = (TGPC/TC)
+  
+  print("Your GPA is: " + str(GPA))
   
 
 if __name__ == "__main__":
